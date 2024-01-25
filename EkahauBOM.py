a0ea727a41c6cf09ba2ec54e88797c6581117bbd
psemenischev <psemenischev@yandex-team.ru>
2024-01-25 14:01:04 +0300

    The "color" attribute has been added

--- EkahauBOM.py
+++ EkahauBOM.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import csv
 import sys
 from zipfile import ZipFile
@@ -8,8 +11,9 @@ with ZipFile(sys.argv[1]) as f:
     access_points = json.loads(f.read("accessPoints.json"))
     vendor_model = []
     for ap in access_points["accessPoints"]:
+        vendor = ap["vendor"]
         if ap["mine"]:
-            vendor_model += [(ap["vendor"], ap["model"])]
+            vendor_model += [(ap["vendor"], ap["model"], ap["color"])]
 
     simulated_radios = json.loads(f.read("simulatedRadios.json"))
     antennas = json.loads(f.read("antennaTypes.json"))
@@ -17,13 +21,13 @@ with ZipFile(sys.argv[1]) as f:
     antenna_types = []
     for radio in simulated_radios["simulatedRadios"]:
         for antenna in antennas["antennaTypes"]:
-            if radio["antennaTypeId"] in antenna.values():
+            if radio["antennaTypeId"] == antenna["id"]:
                 antenna_types += [antenna["name"]]
 
 with open("access_points.csv", "w") as ap_csv_f:
     csv_result = csv.writer(ap_csv_f, dialect="excel", quoting=csv.QUOTE_ALL)
     for ap, count in Counter(vendor_model).items():
-        csv_result.writerow([ap[0], ap[1], count])
+        csv_result.writerow([ap[0], ap[1], ap[2], count])
 
 with open("antennas.csv", "w") as ant_csv_f:
     csv_result = csv.writer(ant_csv_f, dialect="excel", quoting=csv.QUOTE_ALL)
