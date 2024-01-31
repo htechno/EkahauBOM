c40eaeb34bf321ef262d00cb47465dd488a71d64
psemenischev <psemenischev@yandex-team.ru>
2024-01-31 14:45:44 +0300

    "Floor name" added

--- EkahauBOM.py
+++ EkahauBOM.py
@@ -15,15 +15,19 @@ color_database = {"#FFE600" : "Yellow", "#FF8500" : "Orange", "#FF0000" : "Red",
 
 with ZipFile(sys.argv[1]) as f:
     access_points = json.loads(f.read("accessPoints.json"))
+    floor_plans = json.loads(f.read("floorPlans.json"))
     vendor_model = []
     for ap in access_points["accessPoints"]:
         if ap["mine"]:
+            for floor in floor_plans["floorPlans"]:
+                if ap["location"]["floorPlanId"] == floor["id"]:
+                    floor_name = floor["name"]
             if "color" in ap.keys():
                 try:
                     color_name = color_database[ap["color"]]
-                    vendor_model += [(ap["vendor"], ap["model"], color_name)]
+                    vendor_model += [(ap["vendor"], ap["model"], color_name, floor_name)]
                 except:
-                    vendor_model += [(ap["vendor"], ap["model"], ap["color"])]
+                    vendor_model += [(ap["vendor"], ap["model"], ap["color"], floor_name)]
             else:
                 vendor_model += [(ap["vendor"], ap["model"], None)]
 
@@ -38,9 +42,9 @@ with ZipFile(sys.argv[1]) as f:
 
 with open(f'output/{Path(sys.argv[1]).stem}_access_points.csv', "w") as ap_csv_f:
     csv_result = csv.writer(ap_csv_f, dialect="excel", quoting=csv.QUOTE_ALL)
-    csv_result.writerow(["Vendor", "Model", "Color", "Quantity"])
+    csv_result.writerow(["Vendor", "Model", "Floor", "Color", "Quantity"])
     for ap, count in Counter(vendor_model).items():
-        csv_result.writerow([ap[0], ap[1], ap[2], count])
+        csv_result.writerow([ap[0], ap[1], ap[3], ap[2], count])
 
 with open(f'output/{Path(sys.argv[1]).stem}_antennas.csv', "w") as ant_csv_f:
     csv_result = csv.writer(ant_csv_f, dialect="excel", quoting=csv.QUOTE_ALL)
